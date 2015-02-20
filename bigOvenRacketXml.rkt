1d5b02340e37e4a7f05ab5ae0040b36ff82cf51e
sjones1272 <stephen.jones@vikings.berry.edu>
2015-02-20 12:15:46 -0500

    updates

--- bigOvenRacketXml.rkt
+++ bigOvenRacketXml.rkt
@@ -39,11 +39,370 @@ From Ingredients we need to pull
 (define (grab-recipe baseUrl id key)
 (xml->xexpr (document-element (read-xml (get-pure-port (string->url (string-append baseUrl "/" id "?api_key=" key)))))))
 
+(define (grab-relevant-info ls)
+  (cons (pull-tag ls 'Title) (cons (pull-tag ls 'Cuisine)
+                                   (cons (pull-tag ls 'ImageUrl)
+ 
+                                         (pull-tag ls 'Ingredients)))))
+#|  
+(cond
+   [(empty? ls) empty]
+   [(and (list? ls) (equal? (first (first ls) 'Ingredients)) (cons (first ls) (grab-relevant-info (rest ls)))]
+   [(equal? (first ls) 'Title) (cons (first ls) (grab-relevant-info (rest ls)))]
+   [(equal? (first ls) 'Cuisine) (cons (first ls) (grab-relevant-info (rest ls)))]
+   [(equal? (first ls) 'ImageUrl) (cons (first ls) (grab-relevant-info (rest ls)))]
+   [else (grab-relevant-info (rest ls))]))
+  |# 
+
+
 ;;## Generic function to pull any outer tag from a given list
 ;;## (pull-tag test 'Ingredients) - pulls all ingredients
 (define (pull-tag l y)
    (filter (lambda (x) (and (list? x) (equal? (first x) y))) l))
 
+(define (pull-ingredients)
+   (first (pull-tag test 'Ingredients)))
+
+(define ingred '((Ingredients
+   ()
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462601")
+    "\r\n      "
+    (DisplayIndex () "0")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Salmon steaks")
+    "\r\n      "
+    (HTMLName
+     ()
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/salmon\" class=\"glosslink\""
+     ">"
+     "Salmon"
+     "<"
+     "/a"
+     ">"
+     " steaks")
+    "\r\n      "
+    (Quantity () "8")
+    "\r\n      "
+    (DisplayQuantity () "8")
+    "\r\n      "
+    (MetricQuantity () "8")
+    "\r\n      "
+    (MetricDisplayQuantity () "8")
+    "\r\n      "
+    (MetricUnit ())
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "Salmon steaks") "\r\n        " (Department () "Meats") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462602")
+    "\r\n      "
+    (DisplayIndex () "1")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "olive oil")
+    "\r\n      "
+    (HTMLName
+     ()
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/olive%20oil\" class=\"glosslink\""
+     ">"
+     "olive oil"
+     "<"
+     "/a"
+     ">")
+    "\r\n      "
+    (Quantity () "1")
+    "\r\n      "
+    (MetricQuantity () "0")
+    "\r\n      "
+    (MetricDisplayQuantity ())
+    "\r\n      "
+    (MetricUnit ())
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "olive oil") "\r\n        " (Department () "Oils") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462603")
+    "\r\n      "
+    (DisplayIndex () "2")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Butter")
+    "\r\n      "
+    (HTMLName () "<" "a href=\"http://www.bigoven.com/glossary/butter\" class=\"glosslink\"" ">" "Butter" "<" "/a" ">")
+    "\r\n      "
+    (Quantity () "0.25")
+    "\r\n      "
+    (DisplayQuantity () "1/4")
+    "\r\n      "
+    (Unit () "c")
+    "\r\n      "
+    (MetricQuantity () "59")
+    "\r\n      "
+    (MetricDisplayQuantity () "59")
+    "\r\n      "
+    (MetricUnit () )
+    "\r\n      "
+    (PreparationNotes () "melted")
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "Butter") "\r\n        " (Department () "Dairy") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462604")
+    "\r\n      "
+    (DisplayIndex () "3")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Lemon juice")
+    "\r\n      "
+    (HTMLName
+     ()
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/lemon\" class=\"glosslink\""
+     ">"
+     "Lemon"
+     "<"
+     "/a"
+     ">"
+     " juice")
+    "\r\n      "
+    (Quantity () "2")
+    "\r\n      "
+    (DisplayQuantity () "2")
+    "\r\n      "
+    (Unit () "tb")
+    "\r\n      "
+    (MetricQuantity () "30")
+    "\r\n      "
+    (MetricDisplayQuantity () "30")
+    "\r\n      "
+    (MetricUnit () "ml")
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "Lemon juice") "\r\n        " (Department () "Produce") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462605")
+    "\r\n      "
+    (DisplayIndex () "4")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Chopped parsley")
+    "\r\n      "
+    (HTMLName
+     ()
+     "Chopped "
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/parsley\" class=\"glosslink\""
+     ">"
+     "parsley"
+     "<"
+     "/a"
+     ">")
+    "\r\n      "
+    (Quantity () "2")
+    "\r\n      "
+    (DisplayQuantity () "2")
+    "\r\n      "
+    (Unit () "tb")
+    "\r\n      "
+    (MetricQuantity () "30")
+    "\r\n      "
+    (MetricDisplayQuantity () "30")
+    "\r\n      "
+    (MetricUnit () "ml")
+    "\r\n      "
+    (IngredientInfo
+     ()
+     "\r\n        "
+     (Name () "Chopped parsley")
+     "\r\n        "
+     (Department () "Produce")
+     "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462606")
+    "\r\n      "
+    (DisplayIndex () "5")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "rosemary, dried")
+    "\r\n      "
+    (HTMLName
+     ()
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/rosemary\" class=\"glosslink\""
+     ">"
+     "rosemary"
+     "<"
+     "/a"
+     ">"
+     ", dried")
+    "\r\n      "
+    (Quantity () "0.25")
+    "\r\n      "
+    (DisplayQuantity () "1/4")
+    "\r\n      "
+    (Unit () "ts")
+    "\r\n      "
+    (MetricQuantity () "1.23223039580426")
+    "\r\n      "
+    (MetricDisplayQuantity () "1")
+    "\r\n      "
+    (MetricUnit () "ml")
+    "\r\n      "
+    (PreparationNotes () "Crushed")
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "rosemary, dried") "\r\n        " (Department () "Spices") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462607")
+    "\r\n      "
+    (DisplayIndex () "6")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Marjoram")
+    "\r\n      "
+    (HTMLName
+     ()
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/marjoram\" class=\"glosslink\""
+     ">"
+     "Marjoram"
+     "<"
+     "/a"
+     ">")
+    "\r\n      "
+    (Quantity () "1")
+    "\r\n      "
+    (MetricQuantity () "0")
+    "\r\n      "
+    (MetricDisplayQuantity ())
+    "\r\n      "
+    (MetricUnit ())
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "Marjoram") "\r\n        " (Department () "Spices") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462608")
+    "\r\n      "
+    (DisplayIndex () "7")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Salt")
+    "\r\n      "
+    (HTMLName () "<" "a href=\"http://www.bigoven.com/glossary/salt\" class=\"glosslink\"" ">" "Salt" "<" "/a" ">")
+    "\r\n      "
+    (Quantity () "0.25")
+    "\r\n      "
+    (DisplayQuantity () "1/4")
+    "\r\n      "
+    (Unit () "ts")
+    "\r\n      "
+    (MetricQuantity () "1.23223039580426")
+    "\r\n      "
+    (MetricDisplayQuantity () "1")
+    "\r\n      "
+    (MetricUnit () "ml")
+    "\r\n      "
+    (IngredientInfo () "\r\n        " (Name () "Salt") "\r\n        " (Department () "Baking") "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n    "
+   (Ingredient
+    ()
+    "\r\n      "
+    (IngredientID () "462609")
+    "\r\n      "
+    (DisplayIndex () "8")
+    "\r\n      "
+    (IsHeading () "false")
+    "\r\n      "
+    (Name () "Coarsely ground pepper")
+    "\r\n      "
+    (HTMLName
+     ()
+     "Coarsely "
+     "<"
+     "a href=\"http://www.bigoven.com/glossary/ground%20pepper\" class=\"glosslink\""
+     ">"
+     "ground pepper"
+     "<"
+     "/a"
+     ">")
+    "\r\n      "
+    (Quantity () "0.125")
+    "\r\n      "
+    (DisplayQuantity () "1/8")
+    "\r\n      "
+    (Unit () "ts")
+    "\r\n      "
+    (MetricQuantity () "0.616115197902132")
+    "\r\n      "
+    (MetricDisplayQuantity () "0.62")
+    "\r\n      "
+    (MetricUnit () "ml")
+    "\r\n      "
+    (IngredientInfo
+     ()
+     "\r\n        "
+     (Name () "Coarsely ground pepper")
+     "\r\n        "
+     (Department () "Spices")
+     "\r\n      ")
+    "\r\n      "
+    (IsLinked () "true")
+    "\r\n    ")
+   "\r\n  ")))
 
 (define test 
 '(Recipe
